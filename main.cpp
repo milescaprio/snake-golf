5a21c7013db3ae135f80e2947a63f98463501fd7
miles <46407361+PunkyMunky64@users.noreply.github.com>
2023-01-08 03:05:56 +0000

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,86 +1,99 @@
 #include <stdio.h>
+#include <cstring>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
 
 #define W 20
 #define H 20
 #define S 3
 
 int d = 1; //-2, -1, 1 left, 2 up
+int score = 0;
 int a_x = 5;
 int a_y = 5;
+char output[H][W];
 
-int (*r)(int max) = &(rand() % max);
-
-enum Thing {
-    Segment,
-    Apple
-}
+auto r = [](int max) -> int{return rand() % max;};
 
 struct Cons {
-    struct Cons* next;
-    struct Cons* printnext;
-    Thing type = Segment;
-
+    struct Cons* next = nullptr;
     int x = 0;
     int y = 0;
     Cons* n(int count) {
-        if (!count) {return;}
+        if (!count) {return this;}
         return n(count - 1);
     }
     Cons* incr() {
-        Cons ret = *this;
+        Cons* ret = new Cons(*this);
         ret->x += d % 2;
         ret->y += d / 2;
+        ret->next = nullptr;
+        return ret;
     }
 };
 
 Cons* gen_Cons(int count) {
     Cons* ret = new Cons;
     if (count) 
-        ret->readnext = ret->next = gen_Cons(count - 1);
+        ret->next = gen_Cons(count - 1);
     return ret;
 }
 
 Cons* back = gen_Cons(S); //a linked list order for left to right top to bottom
-Cons* front = back.n(S);
-Cons* printfront = 
+Cons* front = back->n(S);
 
 void clrscr() {
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
     
-void loop() {
-    chrono::sleep_for(milliseconds(200))
-    clrscr();
-    if (front->x == ax && front->y == ay) {
-        //apple
-        score++;
-        ax = r(s); ay = r(s);
-        return;
+void game() {
+    while(true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        clrscr();
+        if (front->x == a_x && front->y == a_y) {
+            //eat apple
+            score++;
+            a_x = r(W); a_y = r(H);
+            continue;
+        }
+        //here we need to:
+        //-gen new seg in front
+        //-set it as next to last seg
+        //-if not eaten apple, 
+            //-free back and set new back to its ptr simulatenously
+        front->next = front->incr();
+        front = front->next;
+        Cons* temp = back;
+        back = back->next;
+        delete temp;
+        Cons* curr = back;
+        while (curr) {
+            char &p = output[curr->y][curr->x];
+            if (p == 'O') {
+                break; //die
+            }
+            p = 'O';
+            curr->next;
+        }
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                printf("%c", output[i][j]);
+            }
+            printf("\n");
+        }
     }
-    //here we need to:
-    //-gen new seg in front
-    //-set it as next to last seg
-    //-put it in front of correct printseg (hard), don't worry abt efficiency really
-    //-put it in front of //left off thinking about this
-    front->next = incr();
-    if (front->printnext = 
-    front = front->next;
-    back = back-> next;
-    
 }
 
 
 int main(void) {
-    for(int i = 0; i < 100; i++)    {
-        clrscr();
-        printf("Bye   World\r");
-        printf("Hello");
-        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    } 
-  return 0;
+    memset(output,' ', W*H);
+    // for(int i = 0; i < 100; i++)    {
+    //     clrscr();
+    //     printf("Bye   World\r");
+    //     printf("Hello");
+    //     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    // }
+    game();
+    return score;
 }
