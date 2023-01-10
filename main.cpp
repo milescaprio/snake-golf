77bb94622619f7ab3a0e411e46c7341f664bcb53
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2023-01-09 21:23:19 -0800

    most golf changes done

--- main.cpp
+++ main.cpp
@@ -2,40 +2,36 @@
 #include <chrono>
 #include <thread>
 #include <cstdlib>
-#define W 20
-#define H 20
-#define S 5
 using namespace std;
-
-int d = 1; //-2, -1, 1 left, 2 up
-int score = 0;
-int a_x = 5;
-int a_y = 5;
-char output[H][W];
+int d=1; //-2, -1, 1 left, 2 up
+int score=0;
+int a_x=5;
+int a_y=5;
+char output[20][20];
 char input;
-bool open = true;
+bool open=true;
 
-auto r = [](int max) -> int{return rand() % max;};
-
+int r(){return rand()%20;}
+void loop(int a, void (*f)(int)) {
+    for (int i=0;i<a;i++){f(i);}
+}
 class Cons {
 public:
-    Cons* next = nullptr;
-    int x = 0;
-    int y = 0;
+    Cons* next=0;
+    int x=0;
+    int y=0;
     Cons* n(int count) {
         //indexes the current con by n, like a normal array.
-        if (!count) {return this;}
-        return next->n(count - 1);
+        if(!count){return this;}
+        return next->n(count-1);
     }
-    Cons* incr() {
-        //makes a copy of the current Cons and returns its pointer. 
-        //copy is incremented by the snake direction
-        //link the two together yourself.
-        Cons* ret = new Cons(*this);
-        ret->x += d % 2;
-        ret->y += d / 2;
-        ret->next = nullptr;
-        return ret;
+    void incr() {
+        //makes a copy of the current Cons and links it to the current one
+        //make sure next is 0
+        Cons* ret=new Cons(*this);
+        ret->x+=d%2;
+        ret->y+=d/2;
+        next=ret;
     } 
     void gen_Cons(int count) {
         //make sure to free next before calling
@@ -45,86 +41,69 @@ public:
         // if (count) 
         //     ret->next = gen_Cons(count - 1);
         // return ret;
-        if (!count) {return;}
-        next = incr();
+        if (!count){return;}
+        incr();
         next->gen_Cons(count-1);
     }
 };
 
-
-
 Cons* back = new Cons; //a linked list order for left to right top to bottom 
 Cons* front;
 
-void clrscr() {
-    
-}
-
 void receive(){
     while(1) {
-        char a = getchar();
-        if (!(a == '\n' || a == '\r' || a == ' '))
+        char a=getchar();
+        if (!(a=='\n'||a=='\r'||a==' '))
         input = a;
     }
 }
 
-void game() {
-    thread rec (receive);
-    while(true) {
-        memset(output, ' ', W * H); //clear buffer
+int main(void) {
+    back->gen_Cons(5); //creates S + 1 linked Cons
+    front=back->n(5); //get the address of the last Cons, Sth and there's S+1
+
+    thread rec(receive);
+    while(1) {
+        memset(output,' ',400); //clear buffer
         Cons* curr = back; //start here for iteration
-        int i = 0;
-        while (curr) {
-            i++;
-            if (curr->x > 20 || curr->y > 20 || curr->y < 0 || curr->x < 0) {
-                printf("death");
-                break;
+        while(curr) {
+            char& p=output[curr->y][curr->x];
+            if (curr->x>20||curr->y>20||curr->y<0||curr->x<0) {
+                return score; //die
             }
-            char& p = output[curr->y][curr->x];
             output[a_y][a_x] = 'x';
-            if (p == 'O') {
-                printf("death");
-                break; //die
+            if (p=='O') {
+                return score; //die
             }
-            p = 'O';
-            curr = curr->next;
+            p='O';
+            curr=curr->next;
         }
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                printf("%c", output[i][j]);
+        for (int i=0;i<20;i++) {
+            for (int j=0;j<20;j++) {
+                printf("%c",output[i][j]);
             }
             printf("\n");
         }
 
-        this_thread::sleep_for(chrono::milliseconds(200));
+        this_thread::sleep_for(200ms);
 
         for(int i=0;i<20;i++)printf("\n");
-        switch (input) {
-            case 'd' : d = 1; break;
-            case 'a' : d = -1; break;
-            case 'w' : d = -2; break;
-            case 's' : d = 2; break;
-        }
-        input = 0;
+        if(input=='d')d=1;
+        if(input=='a')d=-1;
+        if(input=='w')d=-1;
+        if(input=='s')d=2;
+        input=0;
 
-        front->next = front->incr(); //create a new front
-        front = front->next; //traverse it
-        if (front->x == a_x && front->y == a_y) {
+        front->incr(); //create a new front
+        front=front->next; //traverse it
+        if(front->x==a_x&&front->y==a_y) {
             //eat apple
             score++;
-            a_x = r(W); a_y = r(H);
+            a_x=r();a_y=r();
             continue;
         }
-        Cons* temp = back; //save old back
-        back = back->next; //traverse back
+        Cons* temp=back; //save old back
+        back=back->next; //traverse back
         delete temp; //free old back
     }
-}
-
-
-int main(void) {
-    back->gen_Cons(S); //creates S + 1 linked Cons
-    front = back->n(S); //get the address of the last Cons, Sth and there's S+1
-    game();
-    return score;
 }
