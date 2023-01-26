6206a686a59835e7f8eaadbddb0aee16545cc898
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2023-01-25 22:09:20 -0800

    removed manyyy chars, changed [20][20] to [400] and its vars, combines statements, shortened ifs, added cool list

--- main.cpp
+++ main.cpp
@@ -3,20 +3,30 @@
 #include <cstring>
 #include <chrono>
 #include <cstdlib>
+
+//-30 bytes if you remove spacing between outputs
+//-14 if you make the screen 3x3 instead, replacing all 400s with 9s and 20s with 3s
+//(a) -9 if scoring is removed (s var)
+//-1 or -2 if you make the output rate verty high
+//-30 if you make the output rate the same speed as your pc
+//-1 if the apple's value is just added/subtracted to BY a one digit number and modded into 400 range
+//-18 if the screen buffer is never cleared :) (not recommended)
+//(b) -27 if the back of the snake stays glued to the ground and keeps lengthening
+//-2 extra if (a) and (b) are both done
+
 using namespace std;
 typedef int I;
-I d=1,s=0,X=5,Y=5,i,j;
-char o[20][20],u,a;
+I d=1,s=0,A=9,i,j;
+char o[400],u,a;
 struct C {
     C*h=0;
-    I x=0,y=0;
+    I l=0;
 };
 C*b=new C;C*f=b;
 void v(){
     for(;;){
         a=getchar();
-        if(a!='\n')
-        u=a;
+        u=a=='\n'?u:a;
     }
 }
 I main(){
@@ -24,36 +34,24 @@ I main(){
     for(;;){
         memset(o,' ',400);
         C*c=b;
-        while(c){
-            char&p=o[c->y][c->x];
-            o[Y][X] = 'x';
-            if (p=='O') {
+        for(;c;){
+            o[A]='x';
+            if (o[c->l]=='O'|f->l>400|f->l<0)
                 exit(s);
-            }
-            p='O';
+            o[c->l]='O';
             c=c->h;
         }
-        for(i=0;i<20;i++){
-            for(j=0;j<20;j++){
-                printf("%c",o[i][j]);
-            }
-            printf("\n");
-        }
+        for(i=0;i<400;i++)
+            printf(i%20?"%c":"%c\n",o[i]);
         this_thread::sleep_for(400ms);
         for(i=0;i<20;i++)printf("\n");
-        if(u=='d')d=1;
-        if(u=='a')d=-1;
-        if(u=='w')d=-2;
-        if(u=='s')d=2;
-        u=0;
+        d=u=u=='d'?1:u=='a'?-1:u=='w'?-20:u=='s'?20:d;
         f->h=new C(*f);
         f=f->h;
-        f->x+=d%2;
-        f->y+=d/2;
-        if(f->x>19|f->y>19|f->y<0|f->x<0)exit(s);
-        if(f->x==X&f->y==Y){
+        f->l+=d;
+        if(f->l==A){
             s++;
-            X=rand()%20;Y=rand()%20;
+            A=rand()%400;
         }else{
             C*p=b;
             b=b->h;
