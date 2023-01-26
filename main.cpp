c6fa6dd7b4fbfebf15e2e51b4ca87d7c65310f0b
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2023-01-25 22:11:24 -0800

    obf 528 bytes

--- main.cpp
+++ main.cpp
@@ -14,48 +14,5 @@
 //(b) -27 if the back of the snake stays glued to the ground and keeps lengthening
 //-2 extra if (a) and (b) are both done
 
-using namespace std;
-typedef int I;
-I d=1,s=0,A=9,i,j;
-char o[400],u,a;
-struct C {
-    C*h=0;
-    I l=0;
-};
-C*b=new C;C*f=b;
-void v(){
-    for(;;){
-        a=getchar();
-        u=a=='\n'?u:a;
-    }
-}
-I main(){
-    thread T(v);
-    for(;;){
-        memset(o,' ',400);
-        C*c=b;
-        for(;c;){
-            o[A]='x';
-            if (o[c->l]=='O'|f->l>400|f->l<0)
-                exit(s);
-            o[c->l]='O';
-            c=c->h;
-        }
-        for(i=0;i<400;i++)
-            printf(i%20?"%c":"%c\n",o[i]);
-        this_thread::sleep_for(400ms);
-        for(i=0;i<20;i++)printf("\n");
-        d=u=u=='d'?1:u=='a'?-1:u=='w'?-20:u=='s'?20:d;
-        f->h=new C(*f);
-        f=f->h;
-        f->l+=d;
-        if(f->l==A){
-            s++;
-            A=rand()%400;
-        }else{
-            C*p=b;
-            b=b->h;
-            delete p;
-        }
-    }
-}
+//528 bytes below
+using namespace std;typedef int I;I d=1,s=0,A=9,i,j;char o[400],u,a;struct C{C*h=0;I l=0;};C*b=new C;C*f=b;void v(){for(;;){a=getchar();u=a=='\n'?u:a;}}I main(){thread T(v);for(;;){memset(o,' ',400);C*c=b;for(;c;){o[A]='x';if(o[c->l]=='O'|f->l>400|f->l<0)exit(s);o[c->l]='O';c=c->h;}for(i=0;i<400;i++)printf(i%20?"%c":"%c\n",o[i]);this_thread::sleep_for(400ms);for(i=0;i<20;i++)printf("\n");d=u=u=='d'?1:u=='a'?-1:u=='w'?-20:u=='s'?20:d;f->h=new C(*f);f=f->h;f->l+=d;if(f->l==A){s++;A=rand()%400;}else{C*p=b;b=b->h;delete p;}}}
