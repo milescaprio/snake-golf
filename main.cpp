2551217c4c095317f5974d34bae44f8e7a79e7e2
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2023-04-15 21:36:31 -0700

    git commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
-#include <thread>  //not included in character count
-#include <stdio.h> //msvc infers, not included in character count
-#include <cstring> //msvc infers, not included in character count
+#include <stdio.h> //msvc infers
+#include <cstring> //msvc infers
+#include <cstdlib> //msvc infers
 #include <chrono>  //not included in character count
-#include <cstdlib> //msvc infers, not included in character count
+#include <thread>  //not included in character count
 
-using namespace std;using I=int;I d=1,s=0,A=9,F=400;char o[400],u,a;struct C{C*h=0;I l=8;};I main(){thread T([]{
-for(;a=getchar();)u=a<97?u:a;});for(C*b=new C,*f=b;memset(o,46,F);){I i=0;for(C*c=b;c;o[c->l]=79,c=c->h)
-if(o[c->l]==79|f->l>=F|f->l<0)exit(s);for(o[A]=120;i<F+20;++i)printf(i<F&&i%20?"%c":"\n%c",(i<F)*o[i]);
-this_thread::sleep_for(200ms);d=(u&2?20:1)*((u&2^u/2&2)-1);f=f->h=new C(*f);if((f->l+=d)==A)++s,A=rand()%F;
-else{C*p=b;b=b->h;delete p;}}}
+/*
+OOOOOOOO
+OOOOOOOO
+OOOOOOOO
+OOOOOOOO
+
+..oooo
+.....o
+oooooo
+o.....
+oo....
+.oo.o.
+|       |       |       |       |       |       |
+*/
+                using namespace std;using I=int;
+                I d=1,s=0,A=9,F=400;char o[400],
+                u,a;struct C{C*h=0;I l=8;};I ///
+                main(){thread T([]{for(;a=//////
+                                        getchar(
+                                        );)u=a//
+                                        <97?u:a;
+                                        });for(C
+*b=new C,*f=b;memset(o,46,F);){I i=0;for(C*c=b;c
+;o[c->l]=79,c=c->h)if(o[c->l]==79|f->l>=F|f->l<0
+)exit(s);for(o[A]=120;i<F+20;++i)printf(i<F&&i//
+%20?"%c":"\n%c",(i<F)*o[i]);this_thread::///////
+sleep_for
+(200ms);
+d=(u&2//
+?20:1)*(
+(u&2^u/2&2)-1);f
+=f->h=new C(*f);
+if((f->l+=d)==A)
+++s,A=rand()%F//
+        ;else{C*p=b;b=/*        */b->h//
+        ;delete p;}}}/*/        Thanks 4
+        playn snake! 457        charsC++
+        (c)MilesCaprio          2023V2.0      */
